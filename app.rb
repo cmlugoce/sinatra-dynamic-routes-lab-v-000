65d125606d80af75a66c2ea05f7c2e451a5134f6
Cristina Lugo <cmlugoce@syr.edu>
2018-07-13 20:21:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,14 +28,14 @@ class App < Sinatra::Base
   
   get get '/:operation/:number1/:number2' do 
     case params[:operation]
-    when 'add'
-      (params[:number1].to_i + params[:number2].to_i).to_s
-    when 'subtract'
-      (params[:number1].to_i - params[:number2].to_i).to_s
-    when 'divide'
-      (params[:number1].to_i / params[:number2].to_i).to_s
-    when 'multiply'
-      (params[:number1].to_i * params[:number2].to_i).to_s
+    when "add"
+      "#{params[:number1].to_i + params[:number2].to_i}"
+    when "subtract"
+      "#{params[:number1].to_i - params[:number2].to_i}"
+    when "multiply"
+      "#{params[:number1].to_i * params[:number2].to_i}"
+    when "divide"
+      "#{params[:number1].to_i / params[:number2].to_i}"
     end
   end
 end 
